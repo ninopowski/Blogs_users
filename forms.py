54ecbae463bbd5ebb82c5e83087866d0fd646ee2
ninopowski <ninoskbd@gmail.com>
2021-12-29 12:48:04 +0100

    Form centered

--- forms.py
+++ forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditorField
 
 class Post(FlaskForm):
-    title = 
+    title = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    body = CKEditorField("Body", validators=[DataRequired()])
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, url_for, request
+from forms import Post
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "verysecretkey"
+Bootstrap(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/new-post")
+@app.route("/new-post", methods=["GET", "POST"])
 def new_post():
-    return render_template("new_post.html")
+    form = Post()
+    return render_template("new_post.html", form=form)
 
 
 if __name__ == "__main__":
