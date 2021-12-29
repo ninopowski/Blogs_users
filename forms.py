38e867872179b083d2870531f05e9a3cd9a0be3a
ninopowski <ninoskbd@gmail.com>
2021-12-29 13:22:18 +0100

    Setting the db up.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_ckeditor import CKEditorField
 
@@ -7,3 +7,4 @@ class Post(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     author = StringField("Author", validators=[DataRequired()])
     body = CKEditorField("Body", validators=[DataRequired()])
+    submit = SubmitField("Add post")
--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from forms import Post
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "verysecretkey"
+
 Bootstrap(app)
+
+#db setup
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Blogs.db"
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    author = db.Column(db.String(150), nullable=False)
+    body = db.Column(db.String, nullable=False)
+
+db.create_all()
+
+
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    posts = BlogPost.query.all()
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/new-post", methods=["GET", "POST"])
 def new_post():
     form = Post()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            author=form.author.data,
+            body=form.body.data
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("home"))
     return render_template("new_post.html", form=form)
 
 
