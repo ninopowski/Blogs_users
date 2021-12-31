36796cc99526b1bd27dd89310af660a075b57efa
ninopowski <ninoskbd@gmail.com>
2021-12-31 10:58:03 +0100

    Second table setup, register form, starting register function

--- forms.py
+++ forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField, PasswordField
 from wtforms.validators import DataRequired
 from flask_ckeditor import CKEditorField
 
-class Post(FlaskForm):
+
+class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     author = StringField("Author", validators=[DataRequired()])
     body = CKEditorField("Body", validators=[DataRequired()])
     submit = SubmitField("Add post")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
-from forms import Post
+from forms import PostForm, RegisterForm
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+#flask setup
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "verysecretkey"
 
@@ -10,19 +12,36 @@ Bootstrap(app)
 
 #db setup
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Blogs.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class BlogPost(db.Model):
+#login menager setup
+login_menager = LoginManager()
+login_menager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), nullable=False)
     author = db.Column(db.String(150), nullable=False)
     body = db.Column(db.String, nullable=False)
 
-#run only onc
+#run only oncw
+#db.create_all()
+
+class User(db.Model):
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String, nullable=False)
+
 db.create_all()
-
-
 
 
 @app.route("/")
@@ -31,9 +50,14 @@ def home():
     return render_template("index.html", posts=posts)
 
 
+@app.route("/register")
+def register():
+    form = RegisterForm()
+    pass
+
 @app.route("/new-post", methods=["GET", "POST"])
 def new_post():
-    form = Post()
+    form = PostForm()
     if form.validate_on_submit():
         new_post = BlogPost(
             title=form.title.data,
